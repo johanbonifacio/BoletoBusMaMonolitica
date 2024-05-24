372a83ccc94d1167735f32c6c9ad6aee4530ec18
johanbonifacio15 <johanalbertobonifacioreyes@gmail.com>
2024-05-23 23:36:30 -0400

    Bus y Asiento - Johan Bonifacio

--- BoletoBusMaMonolitica/BoletoBusMaMonolitica/Data/Entities/Bus.cs
+++ BoletoBusMaMonolitica/BoletoBusMaMonolitica/Data/Entities/Bus.cs
@@ -5,7 +5,9 @@ namespace BoletoBusMaMonolitica.Data.Entities
     public class Bus : BaseEntity
     {
         public int IdBus {  get; set; }
-        public string? NumeroPlaca { get; set; }
+        public string NumeroPlaca { get; set; }
+
+        public string Nombre { get; set; }
         public int CapacidadPiso1 { get; set; }
         public int CapacidadPiso2 { get; set; }
         public int CapacidadTotal { get; set; }
